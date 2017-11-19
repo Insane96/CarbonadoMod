eb975d9aea04aeba960f9ac3ea194d069a533082
Alberto Del Villano <insane96mcp@gmail.com>
2017-11-19 10:55:37 +0100

    1.4.2

--- common/net/insane96mcp/carbonado/Carbonado.java
+++ common/net/insane96mcp/carbonado/Carbonado.java
@@ -16,7 +16,7 @@ public class Carbonado {
 	
 	public static final String MOD_ID = "carbonado";
 	public static final String MOD_NAME = "Carbonado";
-	public static final String VERSION = "1.4.1";
+	public static final String VERSION = "1.4.2";
 	public static final String RESOURCE_PREFIX = MOD_ID.toLowerCase() + ":";
 	public static final String MINECRAFT_VERSIONS = "[1.11,1.11.2]";
 
