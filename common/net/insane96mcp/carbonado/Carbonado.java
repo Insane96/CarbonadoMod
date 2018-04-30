7ac222390abffd6085b4fcf8c681017b71a6891d
Alberto Del Villano <insane96mcp@gmail.com>
2018-04-30 15:06:58 +0200

    1.4.4 - Fixed advancements, added a new one and added proper recipes unlocking

--- common/net/insane96mcp/carbonado/Carbonado.java
+++ common/net/insane96mcp/carbonado/Carbonado.java
@@ -16,7 +16,7 @@ public class Carbonado {
 	
 	public static final String MOD_ID = "carbonado";
 	public static final String MOD_NAME = "Carbonado";
-	public static final String VERSION = "1.4.3";
+	public static final String VERSION = "1.4.4";
 	public static final String RESOURCE_PREFIX = MOD_ID.toLowerCase() + ":";
 	public static final String MINECRAFT_VERSIONS = "[1.12,1.12.2]";
 
