a5ec9baa9db9100d5335c70794f2d45c19d2f1bf
Insane-96 <insane96mcp@gmail.com>
2018-10-27 10:22:23 +0200

    Added names to config

--- common/net/insane96mcp/carbonado/lib/Properties.java
+++ common/net/insane96mcp/carbonado/lib/Properties.java
@@ -13,11 +13,14 @@ public class Properties {
 	
 	public static class ConfigOptions {
 		
+		@Name("Ore Generation")
 		public OreGeneration oreGeneration = new OreGeneration();
 		
 		public static class OreGeneration {
+			@Name("Nether: Ore Per Chunk")
 			@Comment("Ores that try to spawn in a nether chunk, both top and bottom of the nether")
 			public int orePerChunkNether = 4;
+			@Name("Overworld: Ore Per Chunk")
 			@Comment("Ores that try to spawn in a chunk in the overworld")
 			public int orePerChunkOverworld = 6;
 		}
@@ -26,10 +29,13 @@ public class Properties {
 		public Shards shards = new Shards();
 		
 		public static class Shards {
+			@Name("Min Height")
 			@Comment("Anvil minimum height to fall for the carbonado to be destroyed")
 			public int minHeight = 4;
+			@Name("Amount at Min Height")
 			@Comment("Number of shards dropped at min_height, increased by 1 for each block higher")
 			public int countAtMinHeight = 8;
+			@Name("Max Amount")
 			@Comment("Maximum shards that can drop a single carbonado")
 			public int maxCount = 16;
 		}
