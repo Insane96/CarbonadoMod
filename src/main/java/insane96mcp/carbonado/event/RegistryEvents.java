b39adc44314bf3a6c7d743278307933ac71a71ae
Insane96 <insane96mcp@gmail.com>
2020-03-21 16:18:01 +0100

    That unusable axe ... Also nerfed damage

--- src/main/java/insane96mcp/carbonado/event/RegistryEvents.java
+++ src/main/java/insane96mcp/carbonado/event/RegistryEvents.java
@@ -35,18 +35,15 @@ public class RegistryEvents {
 				new Item(new Item.Properties().group(ItemGroup.MATERIALS)).setRegistryName(Carbonado.MOD_ID, Strings.Names.CARBONADO_ITEM),
 				new Item(new Item.Properties().group(ItemGroup.MATERIALS)).setRegistryName(Carbonado.MOD_ID, Strings.Names.CARBONADO_SHARD),
 				new Item(new Item.Properties().group(ItemGroup.MATERIALS)).setRegistryName(Carbonado.MOD_ID, Strings.Names.CARBONADO_SHARD_MOLTEN),
-
 				new ArmorItem(ModMaterial.ARMOR_CARBONADO, EquipmentSlotType.HEAD, new Item.Properties().group(ItemGroup.COMBAT)).setRegistryName(Carbonado.MOD_ID, Strings.Names.CARBONADO_HELMET),
 				new ArmorItem(ModMaterial.ARMOR_CARBONADO, EquipmentSlotType.CHEST, new Item.Properties().group(ItemGroup.COMBAT)).setRegistryName(Carbonado.MOD_ID, Strings.Names.CARBONADO_CHESTPLATE),
 				new ArmorItem(ModMaterial.ARMOR_CARBONADO, EquipmentSlotType.LEGS, new Item.Properties().group(ItemGroup.COMBAT)).setRegistryName(Carbonado.MOD_ID, Strings.Names.CARBONADO_LEGGINGS),
 				new ArmorItem(ModMaterial.ARMOR_CARBONADO, EquipmentSlotType.FEET, new Item.Properties().group(ItemGroup.COMBAT)).setRegistryName(Carbonado.MOD_ID, Strings.Names.CARBONADO_BOOTS),
-
-				new AxeItem(ModMaterial.TOOLS_CARBONADO, 10f, -30.f, new Item.Properties().group(ItemGroup.TOOLS)).setRegistryName(Carbonado.MOD_ID, Strings.Names.CARBONADO_AXE),
+				new AxeItem(ModMaterial.TOOLS_CARBONADO, 6.0f, -3.0f, new Item.Properties().group(ItemGroup.TOOLS)).setRegistryName(Carbonado.MOD_ID, Strings.Names.CARBONADO_AXE),
 				new HoeItem(ModMaterial.TOOLS_CARBONADO, 1.0f, new Item.Properties().group(ItemGroup.TOOLS)).setRegistryName(Carbonado.MOD_ID, Strings.Names.CARBONADO_HOE),
 				new PickaxeItem(ModMaterial.TOOLS_CARBONADO, 1, -2.8f, new Item.Properties().group(ItemGroup.TOOLS)).setRegistryName(Carbonado.MOD_ID, Strings.Names.CARBONADO_PICKAXE),
 				new ShovelItem(ModMaterial.TOOLS_CARBONADO, 1.5f, -3f, new Item.Properties().group(ItemGroup.TOOLS)).setRegistryName(Carbonado.MOD_ID, Strings.Names.CARBONADO_SHOVEL),
 				new SwordItem(ModMaterial.TOOLS_CARBONADO, 3, -2.4f, new Item.Properties().group(ItemGroup.TOOLS)).setRegistryName(Carbonado.MOD_ID, Strings.Names.CARBONADO_SWORD),
-
 				new BlockItem(ModBlocks.carbonadoOre, new Item.Properties().group(ItemGroup.BUILDING_BLOCKS)).setRegistryName(Carbonado.MOD_ID, Strings.Names.CARBONADO_ORE),
 				new BlockItem(ModBlocks.carbonadoBlock, new Item.Properties().group(ItemGroup.BUILDING_BLOCKS)).setRegistryName(Carbonado.MOD_ID, Strings.Names.CARBONADO_BLOCK),
 				new BlockItem(ModBlocks.carbonadoGlass, new Item.Properties().group(ItemGroup.BUILDING_BLOCKS)).setRegistryName(Carbonado.MOD_ID, Strings.Names.CARBONADO_GLASS),
